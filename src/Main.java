d2fc67e3bb770b0d859b4075cca5712cdfa2531f
florian-cassagne <florian.cassagne.pro@gmail.com>
2022-02-28 18:55:32 +0100

    VOiCES

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("FiSSiON");
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Initialiser.initialiser("resources/programme.txt");
+        Lecture.lireAll();
+        System.out.println("FiSSion");
     }
 }
